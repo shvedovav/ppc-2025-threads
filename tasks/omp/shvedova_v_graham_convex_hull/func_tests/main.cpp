e1e01cdd7fe087249a68baba05baf34fe7581e61
shvedovav <vitalina.shvedova@gmail.com>
2025-03-19 06:54:12 +0300

    fix tidy

--- tasks/omp/shvedova_v_graham_convex_hull/func_tests/main.cpp
+++ tasks/omp/shvedova_v_graham_convex_hull/func_tests/main.cpp
@@ -7,6 +7,10 @@
 #include <utility>
 #include <vector>
 
+#ifndef M_PI
+#define M_PI 3.14159265358979323846
+#endif
+
 #include "../include/ops_omp.hpp"
 #include "core/task/include/task.hpp"
 
@@ -291,8 +295,8 @@ TEST(shvedova_v_graham_convex_hull_omp, convex_circle) {
 
   for (int i = 0; i < kNumPoints; ++i) {
     double angle = (2 * M_PI * i) / kNumPoints;
-    src.push_back(kRadius * cos(angle));
-    src.push_back(kRadius * sin(angle));
+    src.push_back(kRadius * std::cos(angle));
+    src.push_back(kRadius * std::sin(angle));
   }
 
   std::vector<double> dst(src.size(), 0.0);
