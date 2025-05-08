e85e04bcbbef5802e9afef13efbb84e50a7e035f
shvedovav <vitalina.shvedova@gmail.com>
2025-05-08 09:12:31 +0300

    fix clang

--- tasks/all/shvedova_v_graham_convex_hull/src/ops_all.cpp
+++ tasks/all/shvedova_v_graham_convex_hull/src/ops_all.cpp
@@ -177,8 +177,7 @@ bool GrahamConvexHullALL::RunImpl() {
         procinput_.resize(div + size);
         MPI_Recv(procinput_.data() + div, size * int(Point{}.size()), MPI_DOUBLE, secondary, 0, group,
                  MPI_STATUS_IGNORE);
-        std::ranges::inplace_merge(procinput_, procinput_.begin() + static_cast<std::int64_t>(div),
-                           comp);
+        std::ranges::inplace_merge(procinput_, procinput_.begin() + static_cast<std::int64_t>(div), comp);
       }
     } else if ((rank_ % i) == 0) {
       const auto size = std::int32_t(procinput_.size());
@@ -250,22 +249,24 @@ void GrahamConvexHullSequential::PerformSort() {  // NOLINT(*cognit*)
       if (dx == 0. && dy == 0.) {
         return -1.;
       }
-      return (dy >= 0) ? (dx >= 0 ? dy / (dx + dy) : 1 - (dx / (-dx + dy)))
+      return (dy >= 0) ? (dx >= 0 ? dy / (dx + dy)  // NOLINT(*nest*)
+                                  : 1 - (dx / (-dx + dy)))
                        : (dx < 0 ? 2 - (dy / (-dx - dy)) : 3 + (dx / (dx - dy)));
     };
     const auto ang1 = calc_ang(p0, p1);
     const auto ang2 = calc_ang(p0, p2);
-    return (ang1 < ang2) || ((ang1 > ang2) ? false
-                                           : (std::pow(p1[0] - p0[0], 2) + std::pow(p1[1] - p0[1], 2) -
-                                                  std::pow(p2[0] - p0[0], 2) - std::pow(p2[1] - p0[1], 2) >
-                                              0));
+    double exp1 = std::pow(p1[0] - p0[0], 2);
+    double exp2 = std::pow(p1[1] - p0[1], 2);
+    double exp3 = std::pow(p2[0] - p0[0], 2);
+    double exp4 = std::pow(p2[1] - p0[1], 2);
+
+    return (ang1 < ang2) || ((ang1 > ang2) ? false : (exp1 + exp2 - exp3 - exp4 > 0));
   };
 
   const auto pivot = *std::ranges::min_element(input_, [](auto &a, auto &b) { return a[1] < b[1]; });
   for (int pt = 0; pt < points_count_; pt++) {
     const bool ev = pt % 2 == 0;
-    const int shift = ev ? 0 : -1;
-    const int revshift = ev ? -1 : 0;
+    const auto [shift, revshift] = ev ? std::make_pair(0, -1) : std::make_pair(-1, 0);
     for (int i = 1; i < points_count_ + shift; i += 2) {
       if (cmp(pivot, input_[i - shift], input_[i + revshift])) {
         std::swap(input_[i], input_[i - (ev ? 1 : -1)]);
